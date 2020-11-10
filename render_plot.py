1cf12da9699152ad3877806ae35c48d24d4da35e
andrey @macOS <me@aadnreev.net>
2020-11-09 22:13:33 -0800

    add light plotting

--- render_plot.py
+++ render_plot.py
@@ -18,27 +18,42 @@ def SaveCapLogPNG(fname):
           #print(cap_line)
           dte  = temp_line[0] # date
           t  = temp_line[1] # cap in
-          data.append([dte, t])
+          if len(temp_line) > 2:
+              light = temp_line[2]
+          else:
+              light = 0
+          data.append([dte, t, light])
 
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+    fig, ax = plt.subplots()
+
     dtes = []
     dtes_str = []
     ts = []
-    for l in data:
-        dtes.append(date2num(datetime.strptime(l[0], '%Y-%m-%d %H:%M:%S.%f')))
-        dtes_str.append(datetime.strptime(l[0], '%Y-%m-%d %H:%M:%S.%f'))
-        ts.append(float(l[1]))
+    ls = []
+    for li in data:
+        dtes.append(date2num(datetime.strptime(li[0], '%Y-%m-%d %H:%M:%S.%f')))
+        dtes_str.append(datetime.strptime(li[0], '%Y-%m-%d %H:%M:%S.%f'))
+        ts.append(float(li[1]))
+        ls.append(float(li[2]))
+    color = 'tab:blue'
 
     ax.set_xticks(dtes[0::int(len(dtes)/10)])
     ax.set_xticklabels([d.strftime('%H:%M') for d in dtes_str[0::int(len(dtes)/10)]])
-    ax.plot(dtes, ts,'--', label='Temperature on '+ d.strftime("%y%m%d"))
+    d = dtes_str[0]
+    ax.plot(dtes, ts,'--', label='Temperature on '+ d.strftime("%y%m%d"),color=color)
 
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(loc=2)
-    ax.set_ylabel('Temperature, deg C')
+    ax.set_ylabel('Temperature, deg C',color=color)
     ax.set_xlabel('Date')
+
+    color = 'tab:red'
+    ax2 = ax.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2.set_ylabel('Light', color=color)  # we already handled the x-label with ax1
+    ax2.plot(dtes, ls,'--', label='Light on '+ d.strftime("%y%m%d"),color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
 
     fig.autofmt_xdate()
 
